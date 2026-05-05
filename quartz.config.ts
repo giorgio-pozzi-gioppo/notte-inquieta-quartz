39d3d355150ed48364eeaa645fbf85ab4cc56bb1
giorgio-pozzi-gioppo <robadigioppo@gmail.com>
2026-05-05 11:35:38 +0200

    Aggiornamento lingua

--- quartz.config.ts
+++ quartz.config.ts
@@ -13,7 +13,7 @@ const config: QuartzConfig = {
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
-    locale: "en-US",
+    locale: "it-IT",
     baseUrl: "giorgio-pozzi-gioppo.github.io/notte-inquieta-quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
